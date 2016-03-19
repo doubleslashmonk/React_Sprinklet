3895b760e5d8e773644edfb261a140719a8799ce
Mudassir Malik <Mudassir.malik@live.com>
2016-03-19 11:49:18 +0530

    Component validation using prototypes

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,10 +1,16 @@
 var React = require('react');
 
 var Repos = React.createClass({
+  propTypes : {
+    username : React.PropTypes.string.isRequired,
+    repos : React.PropTypes.array.isRequired
+  },
+
   render : function(){
     return (
       <div>
-          Repos
+          <h3> Repos </h3>
+          <p> {this.props.repos} </p>
       </div>
     )
   }
--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -1,13 +1,20 @@
 var React = require('react');
 
 var UserProfile = React.createClass({
+  propTypes : {
+    username : React.PropTypes.string.isRequired,
+    bio : React.PropTypes.object.isRequired
+  },
+
   render : function(){
     return (
       <div>
-          User Profile
+          <h3> User Profile </h3>
+          <p> Username : {this.props.username} </p>
+          <p> Bio : {this.props.bio.name} </p>
       </div>
     )
   }
-});  
+});
 
 module.exports = UserProfile;
